d9381eaa1494f1599fc5aa78b848dc35c01d23cb
Daniel Stainback <torann@gmail.com>
2017-10-06 10:26:01 -0400

    Add handle method in order to delegate to fire method for Laravel 5.5

--- src/Commands/RefreshCommand.php
+++ src/Commands/RefreshCommand.php
@@ -44,11 +44,21 @@ class RefreshCommand extends Command
     }
 
     /**
-     * Execute the console command.
+     * Execute the console command for Laravel 5.4 and below
      *
      * @return void
      */
     public function fire()
+    {    
+        $this->handle();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
     {
         $this->info('Refreshing uploaded images...');
         $this->imageRefreshService->refresh($this->argument('class'), $this->option('attachments'));
